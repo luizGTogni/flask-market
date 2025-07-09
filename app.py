bfd3df0e569df0fc51322411cea13ed2333ee913
Luiz Togni <contato.togni@gmail.com>
2025-07-08 23:01:48 -0300

    feat: add market route

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
   return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+  items = [
+    { 'id': 1, 'name': 'Phone', 'barcode': '893524418515', 'price': 500 },
+    { 'id': 2, 'name': 'Laptop', 'barcode': '125485245256', 'price': 900 },
+    { 'id': 3, 'name': 'Keyboard', 'barcode': '854226555545', 'price': 150 }
+  ]
+
+  return render_template('market.html', items=items)
