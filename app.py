b67d500b6602f91dc272ca122ab73d7b13a883cb
Luiz Togni <contato.togni@gmail.com>
2025-07-08 23:50:57 -0300

    feat: add sqlite database

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+  id = db.Column(db.Integer(), primary_key=True)
+  name = db.Column(db.String(length=30), nullable=False, unique=True)
+  barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+  price = db.Column(db.Integer(), nullable=False)
+  description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+  def __repr__(self):
+        return f'Item {self.name}'
 
 @app.route('/')
 @app.route('/home')
@@ -8,10 +22,6 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-  items = [
-    { 'id': 1, 'name': 'Phone', 'barcode': '893524418515', 'price': 500 },
-    { 'id': 2, 'name': 'Laptop', 'barcode': '125485245256', 'price': 900 },
-    { 'id': 3, 'name': 'Keyboard', 'barcode': '854226555545', 'price': 150 }
-  ]
+  items = Item.query.all()
 
   return render_template('market.html', items=items)
