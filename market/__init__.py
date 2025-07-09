08a54bf5ace426bb455baa2303e77c10c279f12c
Luiz Togni <contato.togni@gmail.com>
2025-07-09 16:34:49 -0300

    refactor: add enviroment variables in app.config

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,13 @@
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 db = SQLAlchemy(app)
 
 from market import routes
