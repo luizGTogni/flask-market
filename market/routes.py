35892bcd36280a5117358628f6f66b56d9c01d61
Luiz Togni <contato.togni@gmail.com>
2025-07-09 16:35:26 -0300

    feat: add register form page

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,6 @@
 from market import app
 from market.models import Item
+from market.forms import RegisterForm
 from flask import render_template
 
 @app.route('/')
@@ -12,3 +13,9 @@ def market_page():
   items = Item.query.all()
 
   return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+  form = RegisterForm()
+
+  return render_template('register.html', form=form)
