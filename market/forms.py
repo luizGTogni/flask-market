703825854f39f69c430fe6be02db8a45dc3cfded
Luiz Togni <contato.togni@gmail.com>
2025-07-11 11:45:12 -0300

    feat: add validation messages with flashes

--- market/forms.py
+++ market/forms.py
@@ -1,6 +1,7 @@
+from market.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, Email, EqualTo, DataRequired
+from wtforms.validators import Length, Email, EqualTo, DataRequired, ValidationError
 
 class RegisterForm(FlaskForm):
   username = StringField(label='Username:', validators=[Length(min=3, max=30), DataRequired()])
@@ -8,3 +9,8 @@ class RegisterForm(FlaskForm):
   password1 = PasswordField(label='Password:', validators=[Length(min=8), DataRequired()])
   password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
   submit = SubmitField(label='Create Account')
+
+  def validate_username(self, username_to_check):
+    user = User.query.filter_by(username=username_to_check).first()
+    if user:
+      raise ValidationError('Username already exists! Pleasy try a different username')
